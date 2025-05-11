58d054f901597516f5ebb1907fc555138e57472e
Ibrahim Jimoh <i.jimoh@cloud-tech.africa>
2025-05-11 18:27:33 +0300

    env update

--- infra/dev/backend.tf
+++
@@ -1,9 +0,0 @@
-# terraform {
-#   backend "s3" {
-#     bucket         = "terraform-state-bucket-dev" # Unique to the dev account
-#     key            = "dev/terraform.tfstate"
-#     region         = "us-east-1"
-#     use_lockfile   = true
-#     encrypt        = true
-#   }
-# }
