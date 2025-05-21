2590f613f2b8caa7b737d6c992bf8d8122e54129
Ibrahim Jimoh <i.jimoh@cloud-tech.africa>
2025-05-21 18:24:12 +0300

    add DEVOPS - saul

--- modules/amplify/output.tf
+++ modules/amplify/output.tf
@@ -1,13 +1,13 @@
 output "amplify_frontend_apps" {
     description = "Map of Amplify frontend applications"
     value = {
-        for k, v in var.amp_config : k => aws_amplify_app.frontend[k.name].name
+        for k, v in var.amp_config : k => aws_amplify_app.frontend[k].name
     }
 }
 
 output "amplify_backend_environments" {
     description = "Map of Amplify backend environments"
     value = {
-        for k, v in var.amp_config : k => aws_amplify_backend_environment.backend[k.name].name
+        for k, v in var.amp_config : k => aws_amplify_backend_environment.backend[k].name
     }
 }
