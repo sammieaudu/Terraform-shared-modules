65fe050317825a84ca9412d2329bd56ba7759d2a
Ibrahim Jimoh <i.jimoh@cloud-tech.africa>
2025-05-23 16:52:41 +0300

    ECR Module - Updates

---
+++ modules/ecr/local.tf
@@ -0,0 +1,7 @@
+locals {
+  tags = {
+    Terraform   = "true"
+    Environment = var.env
+    Owner = var.env
+  }
+}
--- modules/ecr/main.tf
+++ modules/ecr/main.tf
@@ -1,8 +1,33 @@
-resource "aws_ecr_repository" "this" {
-  name                 = var.name
-  image_tag_mutability = var.image_tag_mutability
-  image_scanning_configuration {
-    scan_on_push = var.scan_on_push
-  }
-  tags = var.tags
+resource "aws_ecr_account_setting" "registry_policy_scope" {
+  name  = "REGISTRY_POLICY_SCOPE"
+  value = "V2"
 }
+
+resource "aws_ecr_registry_scanning_configuration" "this" {
+  scan_type = "ENHANCED"
+
+  rule {
+    scan_frequency = "SCAN_ON_PUSH"
+    repository_filter {
+      filter      = "*"
+      filter_type = "WILDCARD"
+    }
+  }
+}
+
+resource "aws_ecr_repository" "this" {
+  name                 = var.env
+  image_tag_mutability = "MUTABLE"
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+  tags_all = local.tags
+}
+
+resource "aws_ecr_lifecycle_policy" "this" {
+  repository = aws_ecr_repository.this.name
+  policy = templatefile("${path.module}/policy/lifecycle.json", {})
+
+  depends_on = [ aws_ecr_repository.this ]
+}
+
